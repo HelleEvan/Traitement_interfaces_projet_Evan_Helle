efd4472dc05d99f953e3bda7709cd2760aac4818
Evan Helle <eh648454@ad.u-bourgogne.fr>
2025-11-03 17:26:56 +0100

    avancement fsm

--- Controller_SRAM_Helle/Controller_SRAM_Helle.srcs/sources_1/new/SRAM_controller.vhd
+++ Controller_SRAM_Helle/Controller_SRAM_Helle.srcs/sources_1/new/SRAM_controller.vhd
@@ -33,60 +33,132 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity SRAM_controller is
     Port ( Clk : in STD_LOGIC;
-           Read : in STD_LOGIC;
-           Write : in STD_LOGIC;
+           Ctrl : in STD_LOGIC; -- read and write permet en un seul port. permet d'éviter un read et write en même temps.
+           Start :in STD_LOGIC; -- mettre à 1 pour sortir du Idle
            User_Address : in STD_LOGIC_VECTOR (18 downto 0);
            User_Data_in : in STD_LOGIC_VECTOR (31 downto 0);
            User_Data_out : out STD_LOGIC_VECTOR (35 downto 0);
-           --CTRL : out STD_LOGIC_VECTOR ();
            Address : out STD_LOGIC_VECTOR(18 downto 0);
-           Data : inout STD_LOGIC_VECTOR(35 downto 0));
+           Data : inout STD_LOGIC_VECTOR(35 downto 0);
+           reset : in STD_LOGIC);
 end SRAM_controller;
 
 
 architecture Behavioral of SRAM_controller is
-    
-    signal DATA_whithin : STD_LOGIC_VECTOR (35 downto 0);
-    signal reset : STD_LOGIC;
-    type StateType is(INIT, IDLE, READ, WRITE);
-    signal present_state, next_state : StateType;
-    
-    component bascule_D 
-        Port ( d : in STD_LOGIC;
-               clk : in STD_LOGIC;
-               rst : in STD_LOGIC;
-               q : out STD_LOGIC);
-    end component; 
-    
-begin
-    BD: for I in 0 to 31 generate
-    BDX: bascule_D   port map(
-        q => User_Data_out(I),
-        clk => Clk, 
-        rst => reset,
-        d => User_Data_in(I) 
-        );
-end generate;
--- Instanciation du controleur à faire
 
+    SIGNAL CLKO_SRAM :  std_logic := '0';
+	SIGNAL nCKE :  std_logic := '0';
+	SIGNAL nADVLD :  std_logic := '0';
+	SIGNAL nRW:  std_logic := '0';
+	SIGNAL nOE:  std_logic := '0';
+	SIGNAL nCE:  std_logic := '0';
+	SIGNAL nCE2:  std_logic := '0';
+	SIGNAL CE2:  std_logic := '0';
+	SIGNAL SA :  std_logic_vector(18 downto 0);
+	SIGNAL ENTREE : std_logic_vector(35 downto 0);
+	SIGNAL Trig : std_logic := '0';
+	
+    signal DATA_whithin : STD_LOGIC_VECTOR (35 downto 0);
+    type StateType is(INIT, IDLE, READ, WRITE);
+    signal state : StateType;
+
+component mt55l512y36f is
+    generic (
+      -- Constant parameters
+      addr_bits : integer := 19;
+      data_bits : integer := 36;
+
+      -- Timing parameters for -10 (100 Mhz)
+      tKHKH : time := 10.0 ns;
+      tKHKL : time := 2.5 ns;
+      tKLKH : time := 2.5 ns;
+      tKHQV : time := 5.0 ns;
+      tAVKH : time := 2.0 ns;
+      tEVKH : time := 2.0 ns;
+      tCVKH : time := 2.0 ns;
+      tDVKH : time := 2.0 ns;
+      tKHAX : time := 0.5 ns;
+      tKHEX : time := 0.5 ns;
+      tKHCX : time := 0.5 ns;
+      tKHDX : time := 0.5 ns
+      );
+
+    -- Port Declarations
+    port (
+      Dq    : inout std_logic_vector (data_bits - 1 downto 0);  -- Data I/O
+      Addr  : in    std_logic_vector (addr_bits - 1 downto 0);  -- Address
+      Lbo_n : in    std_logic;                                  -- Burst Mode
+      Clk   : in    std_logic;                                  -- Clk
+      Cke_n : in    std_logic;                                  -- Cke#
+      Ld_n  : in    std_logic;                                  -- Adv/Ld#
+      Bwa_n : in    std_logic;                                  -- Bwa#
+      Bwb_n : in    std_logic;                                  -- BWb#
+      Bwc_n : in    std_logic;                                  -- Bwc#
+      Bwd_n : in    std_logic;                                  -- BWd#
+      Rw_n  : in    std_logic;                                  -- RW#
+      Oe_n  : in    std_logic;                                  -- OE#
+      Ce_n  : in    std_logic;                                  -- CE#
+      Ce2_n : in    std_logic;                                  -- CE2#
+      Ce2   : in    std_logic;                                  -- CE2
+      Zz    : in    std_logic                                   -- Snooze Mode
+      );
+  end component;
+  
+-- Instanciation du controleur
+begin
     process(Clk)
     begin
         DATA_whithin <= "0000" & User_Data_in; --Ajout des 4 bits de parités dans à la donnée
         User_Data_out <= DATA_whithin(31 downto 0); --Renvoie des 32 bits sur la sortie 
     end process; 
 
-    process(Clk, present_state)
+    process(Clk)
     begin 
-        case present_state is
+        case state is
             When INIT => 
-                next_state  <= IDLE;
+                state  <= IDLE;
                 
             when IDLE =>
-                if read ='1' then 
+                if Ctrl ='1' AND Start ='1' then 
+                    state <= READ;
+                elsif Ctrl ='0' AND Start ='1' then 
+                    state <= WRITE;
+                else 
+                    state <= IDLE;
+                end if;
+                
+            when READ =>
+                if Ctrl ='1' AND Start ='1' then 
+                    state <= READ;
+                elsif Ctrl ='0' AND Start ='1' then 
+                    state <= WRITE;
+                else 
+                    state <= IDLE;
+                end if;
+                
+            when WRITE =>
+                if Ctrl ='1' AND Start ='1' then 
+                    state <= READ;
+                elsif Ctrl ='0' AND Start ='1' then 
+                    state <= WRITE;
+                else 
+                    state <= IDLE;
+                end if;
+                
+        end case;
+    end process;  
+    
+    process(state)
+    begin 
+        case state is
+            When INIT => 
+                --sorties!!!
+            when IDLE =>
+     
             when READ =>
                 
             when WRITE =>
                 
-    end process;    
-
+        end case;
+    end process;  
 end Behavioral;
